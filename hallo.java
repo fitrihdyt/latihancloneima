efc6694f70fc4e1544f4d17cad9ed92b39fee60a
fitrihdyt <fitrinurhidayat42@gmail.com>
2024-09-27 09:51:05 +0700

    Tambah Nama

--- hallo.java
+++ hallo.java
@@ -1,5 +1,9 @@
     public class hallo{
         public static void main(String[] args ){
             System.out.println("Hello World");
+            System.out.println("Who's there?");
+            System.out.println("Ghalda?");
+            System.out.println("Oh hello Ghalda");
+            System.out.println("Welcome to the World");
         }
     }
